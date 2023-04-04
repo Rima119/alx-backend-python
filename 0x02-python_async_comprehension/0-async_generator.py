555bb2cbd07abb4edfbd475855ffedbe17efdeaf
Rim El Filali <rimo1902@gmail.com>
2023-04-03 23:32:13 -0700

    add task 0

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Task 0 Module"""
+import asyncio
+from typing import Generator
+import random
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """
+    loops 10 times, asynchronously wait 1 second each time,
+    yield a random number between 0 and 10
+    """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
